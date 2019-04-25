b5f9449a502944d91c78ff44d180827bed326a93
Uj Levente <asdlevente@gmail.com>
2019-04-25 16:49:17 +0200

    basic repo structure set up

---
+++ src/main/java/com.codecool.duedatecalculator/DueDateCalculatorApp.java
@@ -0,0 +1,12 @@
+package com.codecool.duedatecalculator;
+
+import java.time.LocalDateTime;
+
+public class DueDateCalculatorApp {
+
+    private static final int workingHours = 8;
+
+    public void CalculateDueDate() {
+
+    }
+}
---
+++ src/test/java/com.codecool.duedatecalculator/DueDateCalculatorAppTest.java
@@ -0,0 +1,7 @@
+package com.codecool.duedatecalculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DueDateCalculatorAppTest {
+
+}
